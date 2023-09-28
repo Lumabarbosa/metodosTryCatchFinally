e991a71041d47d2b141916b2551714f487d2733e
Lumabarbosa <140550663+Lumabarbosa@users.noreply.github.com>
2023-09-28 19:58:08 -0300

    Add files via upload

---
+++ tryCatchFinally.js
@@ -0,0 +1,29 @@
+/*function desconto (valor){ //desconto de 15% de no máximo R$ 10,00
+
+    return desc;
+}
+*/
+function precoFinal (valor){
+    return valor - (0.15 * valor);
+}
+
+const prompt = require('prompt-sync')()
+console.log('------------ Simule o valor do desconto ------------')
+const preco = parseFloat(prompt('Informe o preço do produto: '))
+
+// métodos try, catch e finally
+try{
+    function desconto (valor) {
+        let desc = 0.15 * valor;
+        if (desc > 10){
+            throw new Error('Desconto máximo de R$ 10,00.')
+        }
+        desc = 10;
+        console.log(`Você está economizando ${desc}`);
+    }
+    desconto(preco);
+} catch (error) {
+    console.log("Erro:" , error.message);
+} finally {
+    console.log('Bloco "finally" sempre será executado.')
+}
